9523f1f6c59cfb3612da47a7d0de5bc297bdc0ed
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-01 13:57:41 -0300

    Add repository dependencies do ExemplarController

--- src/main/java/br/com/zup/edu/biblioteca/controller/ExemplarController.java
+++ src/main/java/br/com/zup/edu/biblioteca/controller/ExemplarController.java
@@ -3,10 +3,22 @@ package br.com.zup.edu.biblioteca.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.zup.edu.biblioteca.repository.ExemplarRepository;
+import br.com.zup.edu.biblioteca.repository.LivroRepository;
+
 @RestController
 @RequestMapping(LivroController.BASE_URI + "/{livroIsbn}" + ExemplarController.BASE_URI)
 public class ExemplarController {
 
     public final static String BASE_URI = "/exemplares";
+
+    private final LivroRepository livroRepository;
+    private final ExemplarRepository exemplarRepository;
+
+    public ExemplarController(LivroRepository livroRepository,
+                              ExemplarRepository exemplarRepository) {
+        this.livroRepository = livroRepository;
+        this.exemplarRepository = exemplarRepository;
+    }
 
 }
