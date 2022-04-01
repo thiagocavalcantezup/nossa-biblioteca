40c14a7e40a03ab7fc377eaa4a8d5c66837be8b0
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-01 16:44:09 -0300

    Add Exemplar creation method in controller

--- src/main/java/br/com/zup/edu/biblioteca/controller/ExemplarController.java
+++ src/main/java/br/com/zup/edu/biblioteca/controller/ExemplarController.java
@@ -1,8 +1,22 @@
 package br.com.zup.edu.biblioteca.controller;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.zup.edu.biblioteca.model.Exemplar;
+import br.com.zup.edu.biblioteca.model.ExemplarDTO;
+import br.com.zup.edu.biblioteca.model.Livro;
 import br.com.zup.edu.biblioteca.repository.ExemplarRepository;
 import br.com.zup.edu.biblioteca.repository.LivroRepository;
 
@@ -19,6 +33,27 @@ public class ExemplarController {
                               ExemplarRepository exemplarRepository) {
         this.livroRepository = livroRepository;
         this.exemplarRepository = exemplarRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> create(@PathVariable String livroIsbn,
+                                       @RequestBody @Valid ExemplarDTO exemplarDTO,
+                                       UriComponentsBuilder uriComponentsBuilder) {
+        String isbn = livroIsbn.replaceAll("[^0-9X]", "");
+        Livro livro = livroRepository.findByIsbn(isbn)
+                                     .orElseThrow(
+                                         () -> new ResponseStatusException(
+                                             HttpStatus.NOT_FOUND,
+                                             "Não existe um livro com o ISBN fornecido."
+                                         )
+                                     );
+        Exemplar exemplar = exemplarRepository.save(exemplarDTO.paraExemplar(livro));
+
+        URI location = uriComponentsBuilder.path(
+            LivroController.BASE_URI + "/{livroIsbn}" + BASE_URI + "/{id}"
+        ).buildAndExpand(isbn, exemplar.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
 }
