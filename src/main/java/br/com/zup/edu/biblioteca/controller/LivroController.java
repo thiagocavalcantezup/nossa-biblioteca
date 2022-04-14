8e809ef59b5c46236977d576c72a6f5107d129de
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-14 10:52:06 -0300

    Add delete method to Livro controller

--- src/main/java/br/com/zup/edu/biblioteca/controller/LivroController.java
+++ src/main/java/br/com/zup/edu/biblioteca/controller/LivroController.java
@@ -2,13 +2,18 @@ package br.com.zup.edu.biblioteca.controller;
 
 import java.net.URI;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.zup.edu.biblioteca.model.Livro;
@@ -27,6 +32,7 @@ public class LivroController {
         this.livroRepository = livroRepository;
     }
 
+    @Transactional
     @PostMapping
     public ResponseEntity<Void> create(@RequestBody @Valid LivroDTO livroDTO,
                                        UriComponentsBuilder uriComponentsBuilder) {
@@ -37,6 +43,29 @@ public class LivroController {
                                            .toUri();
 
         return ResponseEntity.created(location).build();
+    }
+
+    @Transactional
+    @DeleteMapping("/{isbn}")
+    public ResponseEntity<Void> delete(@PathVariable String isbn) {
+        String livroIsbn = isbn.replaceAll("[^0-9X]", "");
+        Livro livro = livroRepository.findByIsbn(livroIsbn)
+                                     .orElseThrow(
+                                         () -> new ResponseStatusException(
+                                             HttpStatus.NOT_FOUND,
+                                             "Não existe um livro com o ISBN informado."
+                                         )
+                                     );
+
+        if (livro.isCirculacaoLivre()) {
+            livroRepository.delete(livro);
+            return ResponseEntity.noContent().build();
+        } else {
+            throw new ResponseStatusException(
+                HttpStatus.UNPROCESSABLE_ENTITY,
+                "Impossível excluir um livro que não seja de circulação livre."
+            );
+        }
     }
 
 }
